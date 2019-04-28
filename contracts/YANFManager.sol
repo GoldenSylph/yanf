4a4c9e199ac3cf98a85b2e69399e507dd2c2c166
Oleg Bedrin <numeralhuman@gmail.com>
2019-04-28 16:18:53 +0300

    Adding test html

--- contracts/YANFManager.sol
+++ contracts/YANFManager.sol
@@ -10,6 +10,7 @@ contract YANFManager is Ownable, Pausable {
   struct Feed {
     uint articlesCount;
     mapping(uint => Article) articles;
+    bool initialized;
   }
 
   struct Article {
@@ -37,134 +38,96 @@ contract YANFManager is Ownable, Pausable {
   YANFToken private tokenContract =
     new YANFToken("Yet Another News Feed Token", "YANF", 18);
 
-  function setFeeReceiver(address payable who) public onlyOwner {
+  function setFeeReceiver(address payable who) public onlyOwner whenNotPaused {
     feeReceiver = who;
   }
 
   function publish(string memory title, string memory content_hash, address[] memory coauthors, uint[] memory parts, uint price)
     public
+    whenNotPaused
     returns(bool)
   {
     if (coauthors.length != parts.length) return false;
 
     uint partsSum = 0;
     for (uint i = 0; i < parts.length; i = SafeMath.add(i, 1)) {
-      partsSum += parts[i];
+      partsSum = SafeMath.add(partsSum, parts[i]);
     }
     if (partsSum > price) {
       return false;
     }
 
-    if (feedsIndexes[msg.sender] == 0) {
+    if (!feeds[feedsIndexes[msg.sender]].initialized) {
       feedsCount = SafeMath.add(feedsCount, 1);
-      uint firstFeedIdx = SafeMath.sub(feedsCount, 1);
-      uint firstArticleIdx = SafeMath.sub(feeds[firstFeedIdx].articlesCount, 1);
-      feeds[firstFeedIdx].articlesCount = 1;
-      feeds[firstFeedIdx].articles[firstArticleIdx] = Article(title, content_hash, true, coauthors.length, 0, price);
-      for (uint i = 0; i < coauthors.length; i = SafeMath.add(i, 1)) {
-        feeds[firstFeedIdx].articles[firstArticleIdx].coauthors[coauthors[i]] = parts[i];
-        feeds[firstFeedIdx].articles[firstArticleIdx].coauthorsRanks[i] = coauthors[i];
-      }
-      feedsIndexes[msg.sender] = firstFeedIdx;
+      feedsIndexes[msg.sender] = SafeMath.sub(feedsCount, 1);
+      feeds[feedsIndexes[msg.sender]] = Feed(0, true);
+      feeds[feedsIndexes[msg.sender]].articlesCount = 1;
+      feeds[feedsIndexes[msg.sender]].articles[0] = Article(title, content_hash, true, coauthors.length, 0, price);
     } else {
-      uint newArticleCount = SafeMath.add(feeds[feedsIndexes[msg.sender]].articlesCount, 1);
-      feeds[feedsIndexes[msg.sender]].articlesCount = newArticleCount;
-      feeds[feedsIndexes[msg.sender]].articles[SafeMath.sub(newArticleCount, 1)] = Article(title, content_hash, true, coauthors.length, 0, price);
-      for (uint i = 0; i < coauthors.length; i = SafeMath.add(i, 1)) {
-        feeds[feedsIndexes[msg.sender]].articles[SafeMath.sub(newArticleCount, 1)].coauthors[coauthors[i]] = parts[i];
-        feeds[feedsIndexes[msg.sender]].articles[SafeMath.sub(newArticleCount, 1)].coauthorsRanks[i] = coauthors[i];
-      }
+      uint oldArticlesCount = feeds[feedsIndexes[msg.sender]].articlesCount;
+      feeds[feedsIndexes[msg.sender]].articlesCount = SafeMath.add(oldArticlesCount, 1);
+      feeds[feedsIndexes[msg.sender]].articles[oldArticlesCount] = Article(title, content_hash, true, coauthors.length, 0, price);
     }
     return true;
   }
 
   function setCoauthorPart(address who, uint articleIndex, uint part)
     public
+    whenNotPaused
     returns(bool)
   {
-    if (who == address(0) || feedsIndexes[msg.sender] == 0
-        || feeds[feedsIndexes[msg.sender]].articlesCount == 0
-        || part >= feeds[feedsIndexes[msg.sender]].articles[articleIndex].price) {
+    uint feedIndexOfSender = feedsIndexes[msg.sender];
+    if (who == address(0)
+        || !feeds[feedIndexOfSender].initialized
+        || part >= feeds[feedIndexOfSender].articles[articleIndex].price) {
       return false;
     }
-    feeds[feedsIndexes[msg.sender]].articles[articleIndex].coauthors[who] = part;
+    if (feeds[feedIndexOfSender].articles[articleIndex].coauthors[who] == 0) {
+      uint oldCoauthorsCount = feeds[feedIndexOfSender].articles[articleIndex].coauthorsCount;
+      feeds[feedIndexOfSender].articles[articleIndex].coauthorsCount = SafeMath.add(oldCoauthorsCount, 1);
+      feeds[feedIndexOfSender].articles[articleIndex].coauthorsRanks[who] = oldCoauthorsCount;
+    }
+    feeds[feedIndexOfSender].articles[articleIndex].coauthors[who] = part;
     return true;
   }
 
-  function addCoauthorPart(address who, uint articleIndex, uint part)
-    public
-    returns(bool)
-  {
-    if (!setCoauthorPart(who, articleIndex, part)) {
-      return false;
-    }
-    feeds[feedsIndexes[msg.sender]].articles[articleIndex].coauthorsCount =
-      SafeMath.add(feeds[feedsIndexes[msg.sender]].articles[articleIndex].coauthorsCount, 1);
-    feeds[feedsIndexes[msg.sender]].articles[articleIndex]
-      .coauthorsRanks[feeds[feedsIndexes[msg.sender]].articles[articleIndex].coauthorsCount] = who;
-    return true;
-  }
-
-  function searchArticlesByTitle(string memory title)
+  function searchArticlesByChosenPredicate
+    (
+      string memory title, uint price, address coauthor,
+      bool titlePredicate, bool pricePredicate, bool coauthorPredicate
+    )
     public
     view
+    whenNotPaused
     returns(uint[] memory resArticlesIndexes, uint[] memory resFeedsIndexes)
   {
     uint[] memory resultArticlesIndexes = new uint[](MAX_WINDOW);
     uint[] memory resultFeedsIndexes = new uint[](MAX_WINDOW);
     uint currentArticleIndexCount = 0;
     uint currentFeedIndexCount = 0;
-    for (uint i = 0; i <= SafeMath.sub(feedsCount, 1); i = SafeMath.add(i, 1)) {
-      for (uint j = 0; j < feeds[i].articlesCount; j = SafeMath.add(j, 1)) {
-        if (stringEquals(feeds[i].articles[j].title, title)) {
-          resultFeedsIndexes[currentFeedIndexCount] = i;
-          currentFeedIndexCount = SafeMath.add(currentFeedIndexCount, 1);
-          resultArticlesIndexes[currentArticleIndexCount] = j;
-          currentArticleIndexCount = SafeMath.add(currentArticleIndexCount, 1);
-        }
-      }
-    }
-    return (resultArticlesIndexes, resultFeedsIndexes);
-  }
 
-  function searchArticleByCoauthor(address coauthor)
-    public
-    view
-    returns(uint[] memory resArticlesIndexes, uint[] memory resFeedsIndexes)
-  {
-    uint[] memory resultArticlesIndexes = new uint[](MAX_WINDOW);
-    uint[] memory resultFeedsIndexes = new uint[](MAX_WINDOW);
-    uint currentArticleIndexCount = 0;
-    uint currentFeedIndexCount = 0;
-    for (uint i = 0; i <= SafeMath.sub(feedsCount, 1); i = SafeMath.add(i, 1)) {
-      for (uint j = 0; j < feeds[i].articlesCount; j = SafeMath.add(j, 1)) {
-        if (articleContainsCoauthor(i, j, coauthor)) {
-          resultFeedsIndexes[currentFeedIndexCount] = i;
-          currentFeedIndexCount = SafeMath.add(currentFeedIndexCount, 1);
-          resultArticlesIndexes[currentArticleIndexCount] = j;
-          currentArticleIndexCount = SafeMath.add(currentArticleIndexCount, 1);
-        }
-      }
-    }
-    return (resultArticlesIndexes, resultFeedsIndexes);
-  }
+    uint maxFeedIterations = SafeMath.sub(feedsCount, 1);
+    uint maxArticleIterations = feeds[i].articlesCount;
 
-  function searchArticleByPrice(uint price)
-    public
-    view
-    returns(uint[] memory resArticlesIndexes, uint[] memory resFeedsIndexes)
-  {
-    uint[] memory resultArticlesIndexes = new uint[](MAX_WINDOW);
-    uint[] memory resultFeedsIndexes = new uint[](MAX_WINDOW);
-    uint currentArticleIndexCount = 0;
-    uint currentFeedIndexCount = 0;
-    for (uint i = 0; i <= SafeMath.sub(feedsCount, 1); i = SafeMath.add(i, 1)) {
-      for (uint j = 0; j < feeds[i].articlesCount; j = SafeMath.add(j, 1)) {
-        if (articleHasEqualOrLesserPrice(i, j, price)) {
+    for (uint i = 0; i <= maxFeedIterations; i = SafeMath.add(i, 1)) {
+      for (uint j = 0; j < maxArticleIterations; j = SafeMath.add(j, 1)) {
+        if (
+          (stringEquals(feeds[i].articles[j].title, title) && titlePredicate) ||
+          (articleHasEqualOrLesserPrice(i, j, price) && pricePredicate) ||
+          (articleContainsCoauthor(i, j, coauthor) && coauthorPredicate)
+        ) {
           resultFeedsIndexes[currentFeedIndexCount] = i;
           currentFeedIndexCount = SafeMath.add(currentFeedIndexCount, 1);
+          if (currentFeedIndexCount > MAX_WINDOW) {
+            i = SafeMath.add(maxFeedIterations, 1);
+            j = SafeMath.add(maxArticleIterations, 1);
+          }
           resultArticlesIndexes[currentArticleIndexCount] = j;
           currentArticleIndexCount = SafeMath.add(currentArticleIndexCount, 1);
+          if (currentArticleIndexCount > MAX_WINDOW) {
+            i = SafeMath.add(maxFeedIterations, 1);
+            j = SafeMath.add(maxArticleIterations, 1);
+          }
         }
       }
     }
@@ -174,6 +137,7 @@ contract YANFManager is Ownable, Pausable {
   function searchFeedByAuthor(address author)
     public
     view
+    whenNotPaused
     returns(uint feedIndex)
   {
     return feedsIndexes[author];
@@ -182,6 +146,7 @@ contract YANFManager is Ownable, Pausable {
   function sendContractFees()
     onlyOwner
     public
+    whenNotPaused
     returns(bool)
   {
     feeReceiver.transfer(address(this).balance);
@@ -191,8 +156,12 @@ contract YANFManager is Ownable, Pausable {
   function buy(uint feed, uint article)
     public
     payable
+    whenNotPaused
     returns(bool)
   {
+    if (!feeds[feed].initialized || !feeds[feed].articles[article].initialized) {
+      return false;
+    }
     if (feeds[feed].articles[article].price > 0) {
       if (msg.value >= feeds[feed].articles[article].price + fee) {
         feeds[feed].articles[article].customers[feeds[feed].articles[article].customersCount] = msg.sender;
@@ -217,6 +186,7 @@ contract YANFManager is Ownable, Pausable {
 
   function receiveRoyalty()
     public
+    whenNotPaused
     returns(bool)
   {
     if (tokenContract.balanceOf(msg.sender) == 0) return true;
@@ -224,15 +194,30 @@ contract YANFManager is Ownable, Pausable {
     tokenContract.burnFrom(msg.sender, tokenContract.balanceOf(msg.sender));
   }
 
-  function stringEquals(string memory a, string memory b) private pure returns(bool) {
+  function stringEquals(string memory a, string memory b)
+    private
+    pure
+    whenNotPaused
+    returns(bool)
+  {
     return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
   }
 
-  function articleContainsCoauthor(uint feed, uint article, address who) private view returns(bool) {
+  function articleContainsCoauthor(uint feed, uint article, address who)
+    private
+    view
+    whenNotPaused
+    returns(bool)
+  {
     return feeds[feed].articles[article].coauthors[who] != 0;
   }
 
-  function articleHasEqualOrLesserPrice(uint feed, uint article, uint price) private view returns(bool) {
+  function articleHasEqualOrLesserPrice(uint feed, uint article, uint price)
+    private
+    view
+    whenNotPaused
+    returns(bool)
+  {
     return feeds[feed].articles[article].price <= price;
   }
 
